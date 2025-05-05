341106380ed5045fc79269765b7aaba4f47290e5
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-06 03:38:13 +0530

    Insertion Sort

--- insertion.c
+++ insertion.c
@@ -4,9 +4,19 @@ void insertion(int arr[], int size){
     for(i = 1; i < size; i++){
         int key = arr[i];
         int j = i - 1;
-        while
+        while (j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+
     }
 }
 int main (){
-
+    int arr [] = {5, 4, 3, 2, 1};
+    int i, size = sizeof(arr)/sizeof(arr[0]);
+    insertion(arr, size);
+    for(i =0; i < size; i++){
+        printf("%d", arr[i]);
+    }
 }
