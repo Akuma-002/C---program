ebbdbabd9be1d54ee0a235d5c76ef7d27acc9794
Shinda <sksahilu735@gmail.com>
2025-05-06 03:05:23 +0530

    Insertion sort

--- insertion.c
+++ insertion.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 void insertion(int arr[], int size){
-    
+    int i;
+    for(i = 1; i < size; i++){
+        int key = arr[i];
+        int j = i - 1;
+        while
+    }
 }
 int main (){
 
